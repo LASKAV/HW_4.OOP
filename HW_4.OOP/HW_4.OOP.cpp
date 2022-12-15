1c94722e9c101a1b677ebd501599205083ed24e8
Black Wizard <110311691+LASKAV@users.noreply.github.com>
2022-12-15 18:08:10 +0200

    Update HW_4.OOP.cpp

--- HW_4.OOP/HW_4.OOP.cpp
+++ HW_4.OOP/HW_4.OOP.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURITYCRITICAL_ATTRIBUTE
 #include <iostream>
 #include "Class_Room.h"
 #include "Class_Wallpaper.h"
@@ -20,48 +21,100 @@
 Создать ситуации, когда используется конструктор копирования.
 */
 
-float wallpaper_strips(Wallpaper Len, Room Hei)
+// Периметр комнаты ÷ Ширину рулона обоев = Количество полотнищ обоев.
+void wallpaper_Number_strips(Room para, Wallpaper wid)
 {
-    std::cout << "Enter length wallpaper : ";
-    std::cin >> Len.Length;
-    std::cout << "Enter Height room : ";
-    std::cin >> Hei.Height;
-    float temp = Len.getWallpaper_L() / Hei.getRoom_H();
-    std::cout << "Whole chunks came out  = " << temp;
-    return temp;
+    para.parameters = para.GetRoom_P();
+    wid.Width = wid.GetWallpaper_W();
+    std::cout << "Number of sheets of wallpaper = " << float(para.parameters / wid.Width) << std::endl;
+    // Пример: 18 м (периметр комнаты) ÷ 1,06 м (ширина рулона выбранных обоев) = 16,981.
 }
 
-float wallpaper_Number_strips(Room paramet, Wallpaper width)
+// нужно посчитать, сколько рулонов обоев нужно для оклеивания комнаты
+void wallpaper_strips(Wallpaper Len, Room Hei , Wallpaper cos)
 {
-    std::cout << "Enter paramet Room : ";
-    std::cin >> paramet.parameters;
-    std::cout << "Enter Height wallpaper : ";
-    std::cin >> width.Width;
-    float temp = paramet.parameters / width.Width;
-    std::cout << "Number of strips you need   = " << temp;
-    return temp;
+    Hei.parameters = Hei.GetRoom_P();
+    Len.Width = Len.GetWallpaper_W();
+    cos.cost = cos.GetWallpaper_C();
+    float temp = 0;
+    temp = Hei.parameters / Len.Width;
+    Len.Length = Len.GetWallpaper_L();
+    Hei.Height = Hei.GetRoom_H();
+    std::cout << "Rolls of wallpaper are needed = " << float((temp * Hei.Height + 15) / Len.Length) << std::endl;
+    float Number_rolls = (temp * Hei.Height + 15) / Len.Length;
+    std::cout << "It will cost you = " << Number_rolls * cos.cost << " $" << std::endl;
+    // Количество полотнищ × (Высота стен + 15 см) ÷ Длина рулона обоев = Количество рулонов.
 }
 
 int main()
 {
     Flat Obj_Flat;
-    Obj_Flat.Input_flat();
-    Obj_Flat.OutPut_flat();
-
     Room Obj_Room;
-    Obj_Room.Input_parameters_room();
-    Obj_Room.OutPut_parameters_room();
-    Obj_Room.getRoom_H();
-    Obj_Room.getRoom_P();
-
     Wallpaper Obj_Wallpaper;
-    Obj_Wallpaper.Input_parameters_wallpaper();
-    Obj_Wallpaper.OutPut_parameters_wallpaper();
-    Obj_Wallpaper.getWallpaper_L();
-    Obj_Wallpaper.getWallpaper_H();
+    short batton = NULL;
+    do
+    {
+        std::cout << "\n __Wallpaper cost__ " << std::endl
+            << "1.Enter number of rooms in the apartment" << std::endl                   // количество комнат в квартире
+            << "2.Enter room parameters" << std::endl                                    // параметры комнаты
+            << "3.Enter the parameters for each type of wallpaper" << std::endl          // параметры каждого вида обоев
+            << "4.Required number of bales of each" << std::endl                         // количество рулонов каждого вида
+            << "5.Purchase cost of wallpaper " << std::endl                              // стоимость закупки обоев 
+            << "6.All parameters" << std::endl
+            << "7.EXIT " << std::endl;
+        std::cout << "\n Enter number : "; std::cin >> batton;
+        switch (batton)
+        {
+        case 1:
+        {
+            system("cls");
+            int rooms = NULL;
+            std::cout << "Enter number of rooms : "; std::cin >> rooms;
+            Obj_Flat.Input_flat(rooms);
+            Obj_Flat.OutPut_flat();
+        }break;
+        case 2:
+        {
+            system("cls");
+            Obj_Room.Input_parameters_room();
+            Obj_Room.OutPut_parameters_room();
+        }break;
+        case 3:
+        {
+            system("cls");
+            Obj_Wallpaper.Input_parameters_wallpaper();
+            Obj_Wallpaper.OutPut_parameters_wallpaper();
+        }break;
+        case 4:
+        {
+            system("cls");
+            wallpaper_Number_strips(Obj_Room, Obj_Wallpaper);            // Количество полотнищ обоев
+        }break;
+        case 5:
+        {
+            system("cls");
+            wallpaper_strips(Obj_Wallpaper, Obj_Room, Obj_Wallpaper);    // сколько рулонов обоев нужно для оклеивания комнаты + цена
+        }break;
+        case 6:
+        {
+            std::cout << "\tFLAT" << std::endl;
+            Obj_Flat.OutPut_flat();
+            std::cout << "\tROOM" << std::endl;
+            Obj_Room.OutPut_parameters_room();
+            std::cout << "\tWALLPAPER" << std::endl;
+            Obj_Wallpaper.OutPut_parameters_wallpaper();
+        }break;
+        case 7:
+        {
+            system("puse");
+            return 0;
+        }break;
+        
+        default:
+            break;
+        }
+    } while (batton != 7);
 
-    wallpaper_strips(Obj_Wallpaper, Obj_Room);
-    std::cout << std::endl;
-    wallpaper_Number_strips(Obj_Room, Obj_Wallpaper);
+    return 0;
+
 }
-
